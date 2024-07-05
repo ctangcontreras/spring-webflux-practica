5d93e349d952db8cf84707f2c3c3853a1778f6b6
CTANG-HM\PC <ctangcontreras@gmail.com>
2024-07-05 10:54:03 -0500

    avance

--- src/main/java/com/bezkoder/spring/webflux/config/SecurityConfig.java
+++ src/main/java/com/bezkoder/spring/webflux/config/SecurityConfig.java
@@ -1,21 +1,17 @@
 package com.bezkoder.spring.webflux.config;
-
-
  
-impoimport org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 import com.bezkoder.spring.webflux.handler.JwtWebFilter;
 import com.bezkoder.spring.webflux.security.SecurityContextRepository;
 
 import lombok.AllArgsConstructor;
-
-import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
  
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
@@ -64,3 +60,4 @@ public class SecurityConfig {
                 .build();
     }
     
+}
